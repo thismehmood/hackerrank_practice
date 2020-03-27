e72f2b7c88aef4e9c1439cb385eaea6530509f6a
Mehmood <this.mehmood@gmail.com>
2020-03-27 23:50:46 +0500

    let's_review.cpp

---
+++ Let's_review.cpp
@@ -0,0 +1,37 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+// int main() {
+    
+//         int n;
+//         cin >> n;
+//         for (int i = 0; i < n;  i++){
+             
+//         }
+//     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+//     return 0;
+// }
+int main() {
+    int x;
+    cin >> x;
+    for(int i = 0; i < x; i++){
+        string s;
+        string s1, s2;
+        cin >> s;
+        for(int j=0; j < s.size(); j++){
+            if(j%2==0){
+                s1 += s[j];
+            }else{
+                s2+= s[j];
+            }
+        }
+        cout << s1 << " " << s2 << endl;
+    }
+    return 0;
+}
+
